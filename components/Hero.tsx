f0f1c0dc5a4d492f22311f07c232d415177fa04f
Taha.K <khoumanitaha23@gmail.com>
2023-05-15 16:00:25 +0000

    2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
-import programmingImage from '@/images/ddd.png'
 
 type Props = {}
 
 export default function Hero({}: Props) {
   return (
-    <div className='flex flex-col mx-4 gap-10 lg:mx-24 sm:flex-row'>
+    <div className='flex flex-col mx-4 gap-10 lg:mx-24 sm:flex-row whitespace'> 
         <div className='sm:w-2/3'>
             <h1 className='text-4xl sm:text-5xl font-bold mb-4 text-black' >
                 Transforming Project 
-                <br/>
+                {/* <br/> */}
                 <span className='text-turk1' > Ideas</span>  into Functional 
-                <br/>
+                {/* <br/> */}
                 <span className='text-blue1' > Websites</span>
             </h1>
             <p className='text-sm text-black mb-6'>
@@ -25,9 +24,7 @@ export default function Hero({}: Props) {
                 <i className="fa-solid fa-arrow-down-long"></i>
             </button>
         </div>
-        <div className='mx-auto w-3/4 sm:w-1/3 max-h-full hero-img'>
-            <Image className='mx-auto' src={programmingImage} alt={"programmingImage"} />
-        </div>
+        <div className='mx-auto w-3/4 sm:w-1/3 max-h-full hero-img'></div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/Hero"
 import Navbar from "@/components/Navbar"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-10">
+      <Head>
+        <title>Taha K</title>
+      </Head>
       <Navbar />
       <Hero />
     </div>
